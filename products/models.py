55ff6a313427365134f0c267fe4279d1ec85b6a4
Royal-Aver <royal_aver@rambler.ru>
2025-08-07 11:04:35 +0300

    Lesson 21

--- products/models.py
+++ products/models.py
@@ -10,17 +10,24 @@ class Category(models.Model):
         verbose_name = "Категорию"
         verbose_name_plural = "Категории"
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
-    title = models.CharField(max_length=255, unique=True)
-    slug = models.SlugField(max_length=255, unique=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
-    description = models.TextField()
-    quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to="products_images", blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    slug = models.SlugField(max_length=255, unique=True, verbose_name="URL")
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.0, verbose_name="Цена")
+    discount = models.DecimalField(max_digits=6, decimal_places=2, default=0.0, verbose_name="Скидка")
+    description = models.TextField(verbose_name="Описание")
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    image = models.ImageField(upload_to="products_images", blank=True, null=True, verbose_name="Изображение")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
 
     class Meta:
         db_table = "products"
         verbose_name = "Товар"
         verbose_name_plural = "Товары"
+
+    def __str__(self):
+        return f"{self.title} | {self.price}"
